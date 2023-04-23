8b9ef8dfa33dfe113f8aca969c0390835eead582
Konstantin K <boss.kinzhit@mail.ru>
2023-04-23 13:14:35 +0300

    modified buttons, created animation

---
+++ scripts/slider.js
@@ -0,0 +1,18 @@
+const OPENED_CLASSNAME = 'hydra-tech_open';
+
+const hydraTech = document.querySelector('.js-hydra-tech');
+const hydraTechArrowsBtn = document.querySelector('.js-hydra-tech-arrows__btn');
+
+hydraTechArrowsBtn.addEventListener('click', toggleSlider);
+
+function toggleSlider(){
+    hydraTech.classList.toggle(OPENED_CLASSNAME);
+    
+    if(hydraTech.classList.contains(OPENED_CLASSNAME)){
+        hydraTechArrowsBtn.src = "file:///C:/Users/Honor/Desktop/hydra/resources/icon/arrow-upwards.png";
+    }
+
+    else{
+        hydraTechArrowsBtn.src = "file:///C:/Users/Honor/Desktop/hydra/resources/icon/arrow-down.png";
+    };
+};
